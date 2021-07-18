89482480e2545ed8e16cb2df969e77590753e69c
tdslinden <linden-k@hotmail.com>
2021-07-18 16:17:05 -0700

    Add navigation component and add links

--- app/src/App.js
+++ app/src/App.js
@@ -2,10 +2,12 @@ import { Route, Switch } from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavouritesPage from './pages/Favourites';
+import MainNavigation from './components/MainNavigation';
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path='/' exact>
           <AllMeetupsPage />
