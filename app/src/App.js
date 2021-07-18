5ec61e53a5cd022c22e9e54311927935dcf7fd0a
tdslinden <linden-k@hotmail.com>
2021-07-18 15:57:01 -0700

    Add pages for application. Remove unnecessary code and files for this tutorial. Include react router.

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom'
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavouritesPage from './pages/Favourites';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route path='/'>
+        <AllMeetupsPage />
+      </Route>
+      <Route path='/new-meetups'>
+        <NewMeetupPage />
+      </Route>
+      <Route path='/favourites'>
+        <FavouritesPage />
+      </Route>
     </div>
   );
 }
