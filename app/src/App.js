55ecbac7df0d2a8ee63a8ad49d4a362ce65e1b17
tdslinden <linden-k@hotmail.com>
2021-07-27 20:46:51 -0700

    Improve layout and UI

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,25 @@
-import { Route, Switch } from 'react-router-dom'
-import AllMeetupsPage from './pages/AllMeetups';
-import NewMeetupPage from './pages/NewMeetup';
-import FavouritesPage from './pages/Favourites';
-import MainNavigation from './components/layout/MainNavigation';
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavouritesPage from "./pages/Favourites";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div>
-      <MainNavigation />
-      <Switch>
-        <Route path='/' exact>
-          <AllMeetupsPage />
-        </Route>
-        <Route path='/new-meetup'>
-          <NewMeetupPage />
-        </Route>
-        <Route path='/favourites'>
-          <FavouritesPage />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <AllMeetupsPage />
+          </Route>
+          <Route path="/new-meetup">
+            <NewMeetupPage />
+          </Route>
+          <Route path="/favourites">
+            <FavouritesPage />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
