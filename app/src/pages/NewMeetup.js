532f6de7c7029a7735af80e07dc4bf7e87631d2e
tdslinden <linden-k@hotmail.com>
2021-08-14 17:01:54 -0700

    add handler to send meetup data to Firebase

--- app/src/pages/NewMeetup.js
+++ app/src/pages/NewMeetup.js
@@ -1,10 +1,25 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-    return <section>
-        <h1>Add New Meetup</h1>
-        <NewMeetupForm />
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetup-app-4bfc7-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
+  );
 }
 
 export default NewMeetupPage;
