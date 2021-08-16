f114927a055bc6a1ac30467a1b9aa8e1d7e25323
tdslinden <linden-k@hotmail.com>
2021-08-16 14:19:24 -0700

    Add useEffect and fetch funtionality to read data from Firebase when data is present

--- app/src/pages/NewMeetup.js
+++ app/src/pages/NewMeetup.js
@@ -1,6 +1,9 @@
+import { useHistory } from 'react-router-dom';
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    const history = useHistory();
+
   function addMeetupHandler(meetupData) {
     fetch(
       "https://react-meetup-app-4bfc7-default-rtdb.firebaseio.com/meetups.json",
@@ -11,7 +14,9 @@ function NewMeetupPage() {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then(() => {
+        history.replace('/');
+    });
   }
 
   return (
